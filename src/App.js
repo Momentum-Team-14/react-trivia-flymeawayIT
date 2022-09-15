cfdeec5400ca20beb15215d319796492e2920806
Gary Whitford <ssrdunc@gmail.com>
2022-09-15 13:37:22 -0400

    got categories to display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const [catID, setCatID] = useState(false)
+  
+return (
+  <>
+      <h1>The Trivia Game</h1>
+      <ContentDisplay catID={catID} setCatID={setCatID} />
+    </>
   );
+}
+
+const ContentDisplay = ({quiz, setQuiz}) => {
+  if (!quiz) {
+    return(<DisplayCategories setQuiz={setQuiz} />)
+  }
+  return(<takeQuiz setQuiz={setQuiz} />)
+}
+
+const takeQuiz = ({setCatID}) => {
+  return(
+    <>
+    <div><button onClick={() => {setCatID(false)}}> Return to Categories </button></div>
+    </>
+  )
+}
+const DisplayCategories = ({setCatID}) => {
+  const [categories, setCategories] = useState([])
+  
+  useEffect(() => {
+  console.log('useEffect runs')
+  axios
+  .get('https://opentdb.com/api_category.php')
+  .then((res) => setCategories(res.data.trivia_categories))
+  }, [])
+
+  return (
+    <>
+      <h2>Select a Category</h2>
+      <div className='cat-list'>
+        {console.log('categories runs')}
+        {categories.map((cat) => (
+        <div><button key={cat.id} onClick={() => {setCatID(false)}}> 
+        {cat.name} </button>
+        </div>
+        ))} 
+      </div>
+    </>
+  )
 }
 
 export default App;
